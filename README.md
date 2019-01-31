6ab73195538062674eeae78c0452805a949b8532
Shini Ko <ko.shmily@gmail.com>
2019-01-31 08:39:06 -0500

    Build

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Work in progress...
+# Bullet Journal
+
+This is a text editor built with electron. It is still under development. The end goal is to have it emulate my bullet journal format. Possibily, support plug-ins for custom themes.
+
+### Build
+
+Run<br/>
+`yarn build`
+
+The application can be found in
+`/dist/mac`
--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, dialog, Menu } = require('electron');
 const fs = require('fs');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,7 +17,11 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000/');
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000/'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
   const template = [
     {
